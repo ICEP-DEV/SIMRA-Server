4dafec041ad306fae594544492f2eca5ea811904
Tracy Madela <219244428@tut4life.ac.za>
2023-11-02 10:44:34 +0200

    FIBdata

--- routes/parameters.js
+++ routes/parameters.js
@@ -1,19 +1,108 @@
 const express = require('express');
 const connection = require("../config/config");
 const router = express.Router();
- 
-  
-  
- // Get municipalities within a specific province
-  router.get('/municipalities/:province_id', (req, res) => {
-    const province_Id = req.params.province_id;
-    console.log(req.params.province_id);
-    const query = 'SELECT * FROM municipality WHERE province_id = ?';
-    connection.query(query, province_Id, (err, rows) => {
+
+// Get parameters for a specific pathogen
+router.post('/parameters', (req, res) => {
+  const pathogenName = req.body.pathogenName;
+  const e = 2.718;
+  let pathogenResult = 0;
+  let qslQuery = 0;
+  let indicatorId;
+
+  // Define a function to fetch all data from FIBdata
+  const callAveCount = (callback) => {
+    const sqlQuery = 'SELECT countAverage, indicatorid FROM FIBdata ORDER BY indicatorid DESC LIMIT 1'; // Modify the query to retrieve the latest entry by user_id
+    //const userId = req.user.id; // Replace 'user_id' with the actual user identifier
+    connection.query(sqlQuery, (err, rows) => {
       if (err) {
-        throw err;
+        console.error('Error fetching data from the database:', err);
+        return callback(err, null);
       }
-      res.json(rows);
+
+      if (rows.length > 0) {
+        qslQuery = parseInt(rows[0].countAverage);
+        indicatorId = parseInt(rows[0].indicatorid);
+      }
+
+      callback(null, qslQuery, indicatorId);
+    });
+  };
+
+  // Call the function to fetch indicatorId and countAverage
+  callAveCount((err, qslQuery, indicatorId) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve data from the database' });
+    }
+
+    let alpha, beta, r, k, NFifty;
+    switch (pathogenName) {
+      case "Cryptosporidium parvum":
+        r = 0.059;
+        pathogenResult = 1 - Math.pow(e, -r * qslQuery);
+        break;
+      case "E. coli O157:H7":
+        alpha = 0.4;
+        beta = 54.9;
+        pathogenResult = 1 - Math.pow((1 + qslQuery / beta), -alpha);
+        break;
+      case "Campylobacter jejuni":
+        alpha = 0.145;
+        beta = 7.58;
+        pathogenResult = 1 - Math.pow((1 + qslQuery / beta), -alpha);
+        break;
+      case "Salmonella typhi":
+        alpha = 0.21;
+        beta = 49.78;
+        pathogenResult = 1 - Math.pow((1 + qslQuery / beta), -alpha);
+        break;
+      case "S. Flexneri":
+        alpha = 0.256;
+        beta = 1480;
+        pathogenResult = 1 - Math.pow((1 + qslQuery / beta), -alpha);
+        break;
+      case "Vibrio cholera":
+        alpha = 0.169;
+        beta = 2305;
+        pathogenResult = 1 - Math.pow((1 + qslQuery / beta), -alpha);
+        break;
+      case "Giardia lamblia":
+        k = 0.0199;
+        pathogenResult = 1 - Math.pow(e, -k * qslQuery);
+        break;
+      case "Entamoeba coli":
+        alpha = 0.101;
+        NFifty = 341;
+        pathogenResult = -((1 + qslQuery / NFifty) * Math.pow(2, 1) / 0.101);
+        break;
+      default:
+        alpha = req.body.alpha;
+    }
+
+    pathogenResult = Math.round(pathogenResult * 100) / 100;
+
+    const insertSql = 'INSERT INTO parameters (alpha, beta, r, k, NFifty, pathogenName, indicatorid, pathogenResult) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+    const insertBody = [
+      alpha,
+      beta,
+      r,
+      k,
+      NFifty,
+      pathogenName,
+      indicatorId,
+      pathogenResult,
+    ];
+
+    connection.query(insertSql, insertBody, (err, results) => {
+      if (err) {
+        console.error('Error inserting pathogen:', err);
+        return res.status(500).json({ error: 'Error inserting pathogen' });
+      }
+
+      console.log('Pathogen inserted successfully:', results);
+      res.status(201).send({ message: 'Pathogen added successfully', results });
     });
   });
-  module.exports = router
+});
+
+module.exports = router;
